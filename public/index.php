09503010fa5cb95254e27f285fe705e65e90f56e
Tanatarias <dmilenapc@gmail.com>
2024-08-03 12:22:44 -0500

    this file redirects all requests that do not correspond to an existing file or an existing directory in index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require_once '../config/database.php';
 require_once '../controllers/BirthdayController.php';
 require_once '../models/Birthday.php';
 require_once 'Router.php';
+// Initialize the routes from the api.php file
+require_once '../routes/api.php';
 
 // Create a new instance of the Database class and get the database connection
 $database = new Database();
@@ -15,9 +17,6 @@ $router = new Router();
 
 // Set the database connection in the Router instance
 $router->setConnection($db);
-
-// Initialize the routes from the api.php file
-require_once '../routes/api.php';
 
 // Handle the incoming request based on the request URI
 $router->dispatch($_SERVER['REQUEST_URI']);
