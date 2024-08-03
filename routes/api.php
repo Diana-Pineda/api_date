a64f914a1412bcbd8e2832c4568e2888f0b0060e
Tanatarias <dmilenapc@gmail.com>
2024-08-03 14:53:21 -0500

    Changed API route endpoint from 'cumpleaños' to 'events' in

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/../public/Router.php';
 
 // Define API routes
 $router = new Router();
-$router->get('/cumpleaños', 'BirthdayController@index');
-$router->get('/cumpleaños/{id}', 'BirthdayController@show');
-$router->post('/cumpleaños', 'BirthdayController@store');
-$router->put('/cumpleaños/{id}', 'BirthdayController@update');
-$router->delete('/cumpleaños/{id}', 'BirthdayController@destroy');
+$router->get('/events', 'BirthdayController@index');
+$router->get('/events/{id}', 'BirthdayController@show');
+$router->post('/events', 'BirthdayController@store');
+$router->put('/events/{id}', 'BirthdayController@update');
+$router->delete('/events/{id}', 'BirthdayController@destroy');
